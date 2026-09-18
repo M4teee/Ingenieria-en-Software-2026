0aa8faf4c38798169f7aca8d392ca8208379094b
Lucio Persano <luciopersano0905@gmail.com>
2026-09-18 09:58:07 -0300

    Hola mundo en node

--- Backend/main.js
+++ Backend/main.js
@@ -1,1 +1,10 @@
-//
+const http = require("http");
+
+const servidor = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hola mundo\n");
+});
+
+servidor.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+});
